1837774b3f124f4dcff87a09619f457faf18f9c9
alwinvargheset@outlook.com <alwinvargheset@outlook.com>
2024-09-22 11:29:55 +0530

    cnn

---
+++ streamlit.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import cv2
+import requests
+
+from huggingface_hub import from_pretrained_keras
+
+url = 'https://huggingface.co/username/repo-name/resolve/main/model.h5'
+response = requests.get(url)
+# Save the model locally
+with open('model.h5', 'wb') as f:
+    f.write(response.content)
+
+# Load the saved model
+model = tf.keras.models.load_model('model.h5')
+# Load the pre-trained model
+#model = tf.keras.models.load_model('deep-learning\\CNN\\tumor_detection\\results\\model\\cnn_tumor.h5')
+
+def make_prediction(img, model):
+    img = Image.fromarray(img)
+    img = img.resize((128, 128))
+    img = np.array(img)
+    input_img = np.expand_dims(img, axis=0)
+    input_img = tf.keras.utils.normalize(input_img, axis=1)
+    res = model.predict(input_img)
+    if res > 0.5:
+        return "Tumor Detected"
+    else:
+        return "No Tumor"
+
+# Streamlit app
+st.title("Tumor Detection from MRI Images")
+st.write("Upload a JPG image to detect if it has a tumor.")
+
+uploaded_file = st.file_uploader("Choose a JPG image...", type="jpg")
+
+if uploaded_file is not None:
+    # Convert the file to an opencv image.
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, 1)
+    
+    
+    # Display the uploaded image
+    st.image(img, channels="RGB")
+    
+    # Make prediction
+    result = make_prediction(img, model)
+    st.write(result)
+
+
+
+    
